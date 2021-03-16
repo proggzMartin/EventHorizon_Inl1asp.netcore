253bd5ddfed8851164dcfc1735caab58305f09d4
Martin W <martin.wilhelmsson@utb.ecutbildning.se>
2021-03-16 20:34:56 +0100

    Base-entities added.

---
+++ EventHorizon.Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using EventHorizon.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHorizon.Data
+{
+    public class DataContext : DbContext
+    {
+        DbSet<Attendee> Attendees { get; set; }
+        DbSet<Organizer> Organizers { get; set; }
+        DbSet<Event> Events { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
