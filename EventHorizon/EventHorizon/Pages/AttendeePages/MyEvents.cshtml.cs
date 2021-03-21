1a6455d67640b2cdb3dee95c3d3c4a75ef0ccc65
Martin W <martin.wilhelmsson@utb.ecutbildning.se>
2021-03-21 19:25:05 +0100

    Scaffolded Attendee as List, nly lists users. Added link on firstpage.

---
+++ EventHorizon/EventHorizon/Pages/AttendeePages/MyEvents.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using EventHorizon.Data;
+using EventHorizon.Data.Entities;
+
+namespace EventHorizon.Pages.AttendeePages
+{
+    public class MyEventsModel : PageModel
+    {
+        private readonly EventHorizon.Data.DataContext _context;
+
+        public MyEventsModel(EventHorizon.Data.DataContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Attendee> Attendee { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Attendee = await _context.Attendee.ToListAsync();
+        }
+    }
+}
