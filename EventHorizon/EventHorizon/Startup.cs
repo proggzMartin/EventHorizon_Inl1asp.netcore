a5e34fe3c61c0798dd5ab9c25205f66cef1a4f31
Martin W <martin.wilhelmsson@utb.ecutbildning.se>
2021-03-26 19:40:40 +0100

    Added page rout to Index.

--- EventHorizon/EventHorizon/Startup.cs
+++ EventHorizon/EventHorizon/Startup.cs
@@ -25,7 +25,12 @@ namespace EventHorizon
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddRazorPages(o =>
+            {
+                o.Conventions.AddPageRoute("/Index", "home"); //If user tyoe this, should get to index.
+                o.Conventions.AddPageRoute("/Index", "EventHorizon");
+
+            });
 
             services.AddDbContext<DataContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("EventHorizonContext")));
